ba04dffbf16fef9752d1f86ac4b0a1d2ec64be3a
AndreaPorri <andrea.porri@student.unisi.it>
2024-07-31 20:19:56 +0200

    aggiunta commenti

--- nyst/analysis/speed.py
+++ nyst/analysis/speed.py
@@ -2,24 +2,37 @@ import numpy as np
 
 class FirstSpeedExtractor:
     def __init__(self):
-        self.time_resolutions = [3, 5, 7, 9]
+        # Initialize the class with predefined time resolutions
+        self.time_resolutions = [3, 5, 7, 9] # number of frame window resolution
 
-    def apply(self, position, fps=None):
+    def apply(self, positions, fps=None):
+        # Create an empty dictionary to store speed calculations
         speed_dict = {}
 
+        # Iterate over each time resolution
         for time_resolution in self.time_resolutions:
-            speed_dict[time_resolution] = self.compute_speed(position, time_resolution)
+            # Compute speed for the given position and time resolution
+            speed_dict[time_resolution] = self.compute_speed(positions, time_resolution)
+
+            # If fps is provided, adjust the speed by multiplying with fps
             if fps is not None:
-                speed_dict[time_resolution] *= fps
+                speed_dict[time_resolution] *= fps # units of measurement: position/seconds 
 
         return speed_dict
     
-    def compute_speed(self, position, time_resolution):
-        aug_factor = time_resolution // 2
-        head = np.array([position[0] for _ in range(aug_factor)])
-        tail = np.array([position[-1] for _ in range(aug_factor)])
-        position_aug = np.vstack((head, position, tail))
+    def compute_speed(self, positions, time_resolution):
 
-        speed = (position_aug[aug_factor:] - position_aug[:-aug_factor]) / time_resolution
+        # Calculate augmentation factor as half of the time resolution
+        aug_factor = time_resolution // 2
+
+        # Create arrays for augmenting the start and end of the position array
+        head = np.array([positions[0] for _ in range(aug_factor)])
+        tail = np.array([positions[-1] for _ in range(aug_factor)])
+
+        # Augment the position array by adding head and tail
+        position_aug = np.vstack((head, positions, tail))
+
+        # Compute the speed array. Calculation of the speed array between two positions (first and last) in the scrolling time resolution window.
+        speed = (position_aug[aug_factor:] - position_aug[:-aug_factor]) / time_resolution # units of measurement: position/frame
 
         return speed
--- nyst/pipeline/first_pipeline.py
+++ nyst/pipeline/first_pipeline.py
@@ -73,69 +73,102 @@ class FirstPipeline:
 
         return left_pupil_absolute_position, right_pupil_absolute_position, count_from_lastRoiupd
 
+    
     def run(self, video_path):
+        # Initialize lists to store absolute positions of left and right eye pupils
         left_eye_absolute_positions = []
         right_eye_absolute_positions = []
 
+        # Open the video file   
         cap = cv2.VideoCapture(video_path)
 
+        # Get the frames per second (FPS) and resolution of the video
         fps = cap.get(cv2.CAP_PROP_FPS)
         resolution = (int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)), int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT)))
+        '''
+        if self.video_preprocessing.invert_resolution:
+            resolution = resolution[::-1]
+        '''
 
+
+        # Create a video writer object to save the annotated video
         annotated_video_writer = cv2.VideoWriter("annotated_video.mp4", cv2.VideoWriter_fourcc(*"mp4v"), fps, resolution)
         count_from_lastRoiupd = 0
 
+        # Read the first frame of the video
         ret, frame = cap.read()
         if ret is False:
+            # Raise an error if the frame could not be read
             raise RuntimeError("Error reading video")
 
+        # Apply the processing method for absolute position pupil estimation to the frame
         left_pupil_absolute_position, right_pupil_absolute_position, count_from_lastRoiupd = self.apply(frame,count_from_lastRoiupd,update_roi=True)
 
+        # Append the positions to the respective lists
         left_eye_absolute_positions.append(left_pupil_absolute_position)
         right_eye_absolute_positions.append(right_pupil_absolute_position)
         
+        # Annotate the frame with the pupil positions
         annotated_frame = self.frame_annotator.apply(frame, left_pupil_absolute_position, right_pupil_absolute_position)
 
+        # Display the annotated frame
         # cv2.imshow("frame", annotated_frame)
         cv2.waitKey(1)
+
+        # Write the annotated frame to the video writer
         annotated_video_writer.write(annotated_frame)
 
+
+        # Loop to process each frame of the video
         while True:
+        
+            # Read the next frame of the video
             ret, frame = cap.read()
+            # Break the loop if no frame is read (end of video)
             if ret is False:
                 break
 
+            # Apply the processing method to the frame
             left_pupil_absolute_position, right_pupil_absolute_position, count_from_lastRoiupd = self.apply(frame,count_from_lastRoiupd)
 
+            # Append the positions to the respective lists
             left_eye_absolute_positions.append(left_pupil_absolute_position)
             right_eye_absolute_positions.append(right_pupil_absolute_position)
-            print(left_pupil_absolute_position, right_pupil_absolute_position)
+            print(left_pupil_absolute_position, right_pupil_absolute_position) #Print the positions
 
+            # Annotate the frame with the pupil positions
             annotated_frame = self.frame_annotator.apply(frame, left_pupil_absolute_position, right_pupil_absolute_position)
 
+            # Display the annotated frame
             # cv2.imshow("frame", annotated_frame)
-            # Esce se viene premuto 'q'
+
+            # Exit if 'q' is pressed
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
-
+            
+            # Write the annotated frame to the video writer
             annotated_video_writer.write(annotated_frame)
 
+        # Clean up and release resources
         cv2.destroyAllWindows()
         cap.release()
         annotated_video_writer.release()
 
+        # Convert the positions lists to numpy arrays
         left_eye_absolute_positions = np.array(left_eye_absolute_positions, dtype=np.float32)
         right_eye_absolute_positions = np.array(right_eye_absolute_positions, dtype=np.float32)
 
+        # Extract speed information for the left and right eyes
         left_eye_speed_dict = self.speed_extractor.apply(left_eye_absolute_positions, fps)
         right_eye_speed_dict = self.speed_extractor.apply(right_eye_absolute_positions, fps)
 
-
+        # Combine the speed information into a dictionary
         speed_dict = {
             resolution: {"left": left_eye_speed_dict[resolution], "right": right_eye_speed_dict[resolution]}
             for resolution in left_eye_speed_dict
             }
-
+        
+        # Create the output dictionary with positions and speed information
         output_dict = {
             "position": {
                 "left": left_eye_absolute_positions,
@@ -145,3 +178,4 @@ class FirstPipeline:
         }
 
         return output_dict
+    
